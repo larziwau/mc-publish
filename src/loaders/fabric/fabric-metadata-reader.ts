ef19f1b2340c1eb26ae4f275a243a46c290c779d
larzie <lunar@lavache.com>
2024-12-20 16:38:32 +0300

    reformat

--- src/loaders/fabric/fabric-metadata-reader.ts
+++ src/loaders/fabric/fabric-metadata-reader.ts
@@ -4,13 +4,7 @@ import { LoaderMetadataReader } from "../loader-metadata-reader";
 import { FabricMetadata } from "./fabric-metadata";
 import { FABRIC_MOD_JSON } from "./raw-fabric-metadata";
 
-/**
- * A metadata reader that is able to read Fabric mod metadata from a zipped file.
- */
 export class FabricMetadataReader implements LoaderMetadataReader<FabricMetadata> {
-    /**
-     * @inheritdoc
-     */
     async readMetadataFile(path: PathLike): Promise<FabricMetadata> {
         const metadataText = await readAllZippedText(path, FABRIC_MOD_JSON);
         return FabricMetadata.from(JSON.parse(metadataText));
