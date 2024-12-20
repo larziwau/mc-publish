7c5b664797114b78632153ade434769f7cb54089
larzie <lunar@lavache.com>
2024-12-20 16:27:35 +0300

    Reformat

--- src/games/minecraft/mojang-api-client.ts
+++ src/games/minecraft/mojang-api-client.ts
@@ -4,52 +4,18 @@ import { $i } from "@/utils/collections";
 import { MinecraftVersion, MinecraftVersionManifest, getMinecraftVersionManifestEntries } from "./minecraft-version";
 import { getMinecraftVersionRegExp, normalizeMinecraftVersion, normalizeMinecraftVersionRange } from "./minecraft-version-lookup";
 
-/**
- * The default base URL for the Mojang API.
- */
 export const MOJANG_API_URL = "https://piston-meta.mojang.com/mc";
 
-/**
- * Describes the configuration options for the Mojang API client.
- */
 export interface MojangApiOptions {
-    /**
-     * The Fetch implementation used for making HTTP requests.
-     */
     fetch?: Fetch;
-
-    /**
-     * The base URL for the Mojang API.
-     *
-     * Defaults to {@link MOJANG_API_URL}.
-     */
     baseUrl?: string | URL;
 }
 
-/**
- * A client for interacting with the Mojang API.
- */
 export class MojangApiClient {
-    /**
-     * The Fetch implementation used for making HTTP requests.
-     */
     private readonly _fetch: Fetch;
-
-    /**
-     * A cached map of all available Minecraft versions.
-     */
     private _versions?: ReadonlyMap<string, MinecraftVersion>;
-
-    /**
-     * A cached regular expression for matching Minecraft version strings.
-     */
     private _versionRegExp?: RegExp;
 
-    /**
-     * Creates a new {@link MojangApiClient} instance.
-     *
-     * @param options - The configuration options for the client.
-     */
     constructor(options?: MojangApiOptions) {
         this._fetch = createFetch({
             handler: options?.fetch,
@@ -58,13 +24,6 @@ export class MojangApiClient {
         .use(throwOnError());
     }
 
-    /**
-     * Retrieves a specific Minecraft version by its ID.
-     *
-     * @param id - The ID of the Minecraft version to retrieve.
-     *
-     * @returns A promise that resolves to the Minecraft version, or `undefined` if not found.
-     */
     async getMinecraftVersion(id: string): Promise<MinecraftVersion | undefined> {
         const versions = await this.getAllMinecraftVersions();
         const version = versions.get(id);
@@ -76,13 +35,6 @@ export class MojangApiClient {
         return versionRange[0];
     }
 
-    /**
-     * Retrieves a list of Minecraft versions that match the specified range.
-     *
-     * @param range - A version range to match.
-     *
-     * @returns A promise that resolves to an array of matching Minecraft versions.
-     */
     async getMinecraftVersions(range: string | Iterable<string> | VersionRange): Promise<MinecraftVersion[]> {
         const versions = await this.getAllMinecraftVersions();
         const regex = await this.getMinecraftVersionRegExp();
@@ -91,11 +43,6 @@ export class MojangApiClient {
         return $i(versions.values()).filter(x => normalizedRange.includes(x.version)).toArray();
     }
 
-    /**
-     * Retrieves all available Minecraft versions.
-     *
-     * @returns A promise that resolves to a map of Minecraft versions keyed by their IDs.
-     */
     private async getAllMinecraftVersions(): Promise<ReadonlyMap<string, MinecraftVersion>> {
         if (this._versions) {
             return this._versions;
@@ -115,11 +62,6 @@ export class MojangApiClient {
         return this._versions;
     }
 
-    /**
-     * Retrieves a regular expression for matching Minecraft version strings.
-     *
-     * @returns A promise that resolves to a `RegExp` for matching Minecraft version strings.
-     */
     private async getMinecraftVersionRegExp(): Promise<RegExp> {
         if (this._versionRegExp) {
             return this._versionRegExp;
